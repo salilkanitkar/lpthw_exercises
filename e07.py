b73ca2d2f6547a72b480dc8f3780e77b9cb5d3c7
Salil Kanitkar <slashforu@gmail.com>
2013-10-19 19:58:53 -0700

    Exercises 6 and 7
    Signed-off-by: Salil Kanitkar <slashforu@gmail.com>

---
+++ e07.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+""" Exercise 07 """
+
+print "Mary had a little lamb."
+print "Its fleece was white as %s." % 'snow'
+print "And everywhere that Mary went."
+print "." * 10  # This prints the "." 10 times!
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6,
+# If that "," above is not there at the end of the line, then 
+# below print would go on next line.
+print end7 + end8 + end9 + end10 + end11 + end12
+
+"""
+Test Run:
+$ python e07.py
+Mary had a little lamb.
+Its fleece was white as snow.
+And everywhere that Mary went.
+..........
+Cheese Burger
+"""
+
